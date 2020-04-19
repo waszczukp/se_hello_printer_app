5c0933f150b852f40d808b7fad2c53b5202a95ad
waszczukp <waszczukp@users.noreply.github.com>
2020-04-19 13:41:52 +0200

    formater xml

--- hello_world/formater.py
+++ hello_world/formater.py
@@ -17,12 +17,22 @@ def get_formatted(msg, imie, format):
         result = plain_text_lower_case(msg, imie)
     elif format == JSON:
         result = format_to_json(msg, imie)
+    elif format == XML:
+        result = format_to_xml(msg, imie)
     return result
 
 
 def format_to_json(msg, imie):
     return ('{ "imie":"' + imie + '", "mgs":' +
             msg + '"}')
+
+
+def format_to_xml(msg, imie):
+    return (
+    <greetings>
+        <name>imie</name>
+        <msg>msg</msg>
+    </greetings>)
 
 
 def plain_text(msg, imie):
